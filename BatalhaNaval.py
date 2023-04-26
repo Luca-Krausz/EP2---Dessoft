23956a0183f55862bbcc7619d5170ee0f0c023b2
Luca-Krausz <Krausk225@gmail.com>
2023-04-26 16:15:35 -0300

    Define posições (Pronto)

--- BatalhaNaval.py
+++ BatalhaNaval.py
@@ -1,9 +1,32 @@
-#Bom dia Jun!
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    
+    grid = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+    ]
 
+    posicao = []
 
+    #Se orientação for 'vertical', coluna irá se manter constante
+    #Se orientação for 'horizontal', linha irá se manter constante
 
-# asdasdqq
+    if orientacao == 'vertical':
+        for L in range(linha,linha+tamanho):
+            posicao.append([L,coluna])
 
+    elif orientacao == 'horizontal':
+        for c in range(coluna,coluna+tamanho):
+            posicao.append([linha, c])
+
+    return posicao
 
 
 
